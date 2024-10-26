2a70c6c6da5edd4d2ea33bb8e6ec829ab191010b
tomoda-kazuya <tomoda1207.work@gmail.com>
2024-10-27 02:34:40 +0900

    fix: 🐛 format

--- src/components/PBtn/PBtn.stories.ts
+++ src/components/PBtn/PBtn.stories.ts
@@ -1,7 +1,7 @@
-import { fn } from '@storybook/test';
-import type { Meta, StoryObj } from '@storybook/vue3';
+import { fn } from '@storybook/test'
+import type { Meta, StoryObj } from '@storybook/vue3'
 
-import PBtn from './PBtn.vue';
+import PBtn from './PBtn.vue'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
@@ -18,10 +18,10 @@ const meta = {
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
     onClick: fn(),
   },
-} satisfies Meta<typeof PBtn>;
+} satisfies Meta<typeof PBtn>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 /*
  *👇 Render functions are a framework specific feature to allow you control on how the component renders.
  * See https://storybook.js.org/docs/api/csf
@@ -32,25 +32,25 @@ export const Primary: Story = {
     primary: true,
     label: 'p-btn',
   },
-};
+}
 
 export const Secondary: Story = {
   args: {
     primary: false,
     label: 'p-btn',
   },
-};
+}
 
 export const Large: Story = {
   args: {
     label: 'p-btn',
     size: 'large',
   },
-};
+}
 
 export const Small: Story = {
   args: {
     label: 'p-btn',
     size: 'small',
   },
-};
+}
